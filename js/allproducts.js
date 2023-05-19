09076d2b76a9614a10ff77ee44c47dbd204b226c
ProkopovVsevolod <vaprokopov@mail.ru>
2023-05-19 21:29:35 +0400

    some changes

--- js/allproducts.js
+++ js/allproducts.js
@@ -8,7 +8,7 @@ let product = document.querySelector('.popular-products-cards')
 let idProd = 0;
 
 async function getResponce() {
-    let responce = await fetch(productsUrl);
+    let responce = await fetch(link);
     let content = await responce.json();
     console.log(content);
     let key;
@@ -18,32 +18,33 @@ async function getResponce() {
         if (content[key].action == 'Sale') {
             product.innerHTML += `
             <div class="cards" data-id="${idProd}">
-            <p class="sale">SALE</p>
-            <img class="popular-products-cards-img"  src="${content[key].image}" alt="Рюкзак">
-            <h4 class="title-4">${content[key].model}</h4>
-            <div class="product__price"><span class="product__price-number">${content[key].price}</span> ₽</div>
-            <div class="product__quantity"></div>
-            <button data-cart type="button" class="cart" 
-            data-sb-id-or-vendor-code="${idProd}"
-            data-sb-product-name="${content[key].model}"
-            data-sb-product-price="${content[key].price}"
-            data-sb-product-quantity="1"
-            data-sb-product-img="${content[key].image}"></button>
-        </div>`
+                <p class="sale">SALE</p>
+                <img class="popular-products-cards-img"  src="${content[key].image}" alt="Рюкзак">
+                <h4 class="title-4">${content[key].model}</h4>
+                <div class="product__price"><span class="product__price-number">${content[key].price}</span> ₽</div>
+                <div class="product__quantity"></div>
+                <button data-cart type="button" class="cart" 
+                data-sb-id-or-vendor-code="${idProd}"
+                data-sb-product-name="${content[key].model}"
+                data-sb-product-price="${content[key].price}"
+                data-sb-product-quantity="1"
+                data-sb-product-img="${content[key].image}"></button>
+            </div>`
         } else {
             product.innerHTML += `
             <div class="cards" data-id="${idProd}">
-                    <img class="popular-products-cards-img"  src="${content[key].image}" alt="Рюкзак">
-                    <h4 class="title-4">${content[key].model}</h4>
-                    <div class="product__price"><span class="product__price-number">${content[key].price}</span> ₽</div>
-                    <div class="product__quantity"></div>
-                    <button data-cart type="button" class="cart" 
+                <img class="popular-products-cards-img"  src="${content[key].image}" alt="Рюкзак">
+                <h4 class="title-4">${content[key].model}</h4>
+                <div class="product__price"><span class="product__price-number">${content[key].price}</span> ₽</div>
+                <div class="product__quantity"></div>
+                <button data-cart type="button" class="cart" 
                     data-sb-id-or-vendor-code="${idProd}"
                     data-sb-product-name="${content[key].model}"
                     data-sb-product-price="${content[key].price}"
                     data-sb-product-quantity="1"
-                    data-sb-product-img="${content[key].image}"></button>
-                </div>`
+                    data-sb-product-img="${content[key].image}">
+                </button>
+            </div>`
 
         }
         // if (content[key].type == "Квадроциклы") {
